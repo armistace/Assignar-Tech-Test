bf1f2c8733bc81f05ac187c97ac2da16be3373ac
Andrew Ridgway <armistace87@outlook.com>
2021-06-16 18:23:41 +1000

    finish readme

--- linux_exec.sh
+++ linux_exec.sh
@@ -3,3 +3,5 @@
 docker-compose down
 docker-compose up --build -d
 docker-compose exec assignartest /init_oltp.sh
+docker-compose exec assignartest python3 /code/python/main_etl.py
+docker-compose exec assignartest python3 /code/python/QA.py
